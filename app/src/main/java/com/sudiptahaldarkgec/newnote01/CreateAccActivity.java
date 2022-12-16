3c3771f99c0eacfc0422e7b47ed11214973e36d1
sukto <sudiptahaldar098@gmail.com>
2022-12-16 20:38:14 +0530

    first commit

---
+++ app/src/main/java/com/sudiptahaldarkgec/newnote01/CreateAccActivity.java
@@ -0,0 +1,14 @@
+package com.sudiptahaldarkgec.newnote01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateAccActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_acc);
+    }
+}
